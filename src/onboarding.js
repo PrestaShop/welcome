608a681dd0e27949d8f98124309b8653bf49a161
Julien Martin <martin.julien82@gmail.com>
2016-01-26 16:41:46 +0100

    [+] Babel configuration added
    And OnBoarding class added.

---
+++ src/onboarding.js
@@ -0,0 +1,11 @@
+class OnBoarding
+{
+    construct(currentStep, steps, isShutDown, baseDir, baseAdminDir)
+    {
+        this.currentStep = currentStep;
+        this.steps = steps;
+        this.isShutDown = isShutDown;
+        this.baseDir = baseDir;
+        this.baseAdminDir = baseAdminDir;
+    }
+}
