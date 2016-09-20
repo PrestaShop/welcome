cf18b3a5290746f58c6e325065d0b403ad62f19e
Maxime Biloé <maxime.biloe@prestashop.com>
2016-09-20 16:55:19 +0200

    // Version updated to v1.0.3

--- welcome.php
+++ welcome.php
@@ -47,7 +47,7 @@ class Welcome extends Module
     public function __construct()
     {
         $this->name = 'welcome';
-        $this->version = '1.0.2';
+        $this->version = '1.0.3';
         $this->author = 'PrestaShop';
 
         parent::__construct();
