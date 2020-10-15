416513b2a8b52cc82177ff133c7b05a0b012a56a
Progi1984 <franck.lefevre@prestashop.com>
2020-10-15 10:37:54 +0200

    Bump version to 6.0.2

--- welcome.php
+++ welcome.php
@@ -51,7 +51,7 @@ class Welcome extends Module
     public function __construct()
     {
         $this->name = 'welcome';
-        $this->version = '6.0.1';
+        $this->version = '6.0.2';
         $this->author = 'PrestaShop';
 
         parent::__construct();
