090155b4fc85bfad7de94c7207f1674245079feb
Pierre RAMBAUD <pierre.rambaud86@gmail.com>
2021-06-01 14:19:50 +0200

    Bump to 6.0.5

--- welcome.php
+++ welcome.php
@@ -51,7 +51,7 @@ class Welcome extends Module
     public function __construct()
     {
         $this->name = 'welcome';
-        $this->version = '6.0.4';
+        $this->version = '6.0.5';
         $this->author = 'PrestaShop';
 
         parent::__construct();
